3a4ea13a93fd7acac5c35e6ea02cac7192eae993
Brad <bleisero57@gmail.com>
2017-10-27 19:16:26 -0700

    Working one-client server
    Working http server, print messages are left on.

--- http_server.py
+++ http_server.py
@@ -1,52 +1,67 @@
 import socket
 import os
 
-max_queue_size = 1 # this version of the http server handles 1 client only
-#TODO: use cmd line to request file?
-#ex: http_server.py request.html
-rqpath = "{0}lab2_files{0}{1}.html".format(os.path.sep, )
+max_queue_size = 1
 
-serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Prepare server socket
-host = gethostname() # should be 127.0.0.1 (localhost)
-port = 80
+host = socket.gethostname() # localhost
+port = 666
+print("Server starting: {}:{}\n".format(host, port))
 serverSocket.bind((host, port))
 serverSocket.listen(max_queue_size)
 
 while True:
+    # Establish connection
     print ("Ready to serve...")
     connectionSocket, addr = serverSocket.accept()
-    print ("Connecting to {}".format(addr))
+    print ("Connecting to {}\n".format(addr))
     
     try:
         # Request line
-        message = "GET {0}{1} HTTP/1.1".format(os.path.realpath(__file__), rqpath)
+        message = str(connectionSocket.recv(1024), "utf-8")
+        print("Recieved:\n{}".format(message))
         filename = message.split()[1]
-        f = open(filename[1:], "rb")
-        output = #TODO
+        base = os.path.realpath(os.path.join(os.path.dirname(__file__), "."))
+        filepath = "{2}{0}lab2_files{0}{1}".format(os.path.sep, filename[1:], base)
+        print("Attempting to open {}".format(filepath))
+        
+        if os.path.isfile(filepath):
+            f = open(filepath, "rb")
+            print("File found. Responding...")
+            outputdata = f.read()
+        else:
+            print("Could not find {}. Responding...".format(filename[1:]))
+            raise IOError("404 file not found")
         
         # Send one HTTP header line into socket
-        clientSocket.send(b"header") #TODO
-        clientSocket.send(b"\n")
+        connectionSocket.send(b"HTTP/1.x 200 OK")
+        connectionSocket.send(b'\r\n\r\n') # separates header from body
         
         # Send the content of the requested file to client
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i:i+1])
             
-        connectionSocket.send(b'\r\n\r\n') # blank line?
+        connectionSocket.send(b'\r\n\r\n') # end body
         connectionSocket.close()
     except IOError:
         # Send response message for file not found
+        connectionSocket.send(b"HTTP/1.x 404 NOT FOUND")
+        connectionSocket.send(b'\r\n\r\n')
+
+        # Send HTML for error display
         connectionSocket.send(b"""
 <html>
 <body>
-<h1>Error 404</h1> 404 not found
+<h1>Error 404</h1> 404 file not found
 </body>
 </html>
 """)
         
         # Close client socket
+        connectionSocket.send(b'\r\n\r\n')
         connectionSocket.close()
 
+print("Closing server.")
 serverSocket.close()
